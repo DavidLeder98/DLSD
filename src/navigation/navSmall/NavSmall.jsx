ac8c7833f1915f753004fe1a4c0df453348ce471
David Leder <davidleder98@gmail.com>
2025-04-09 00:46:17 +0200

    ns

--- src/navigation/navSmall/NavSmall.jsx
+++ src/navigation/navSmall/NavSmall.jsx
@@ -1,9 +1,10 @@
 import './NavSmall.css';
 import { useState, useEffect } from "react";
-import Burger from './Burger';
+import { useNavigation } from '../../services/NavigationContext';
 
 const NavSmall = () => {
     const [scrolled, setScrolled] = useState(false);
+    const { scrollToElement } = useNavigation();
 
     useEffect(() => {
         const handleScroll = () => {
@@ -18,14 +19,42 @@ const NavSmall = () => {
         return () => window.removeEventListener("scroll", handleScroll);
     }, []);
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleBurger = () => {
+        setIsOpen(prev => !prev);
+    };
+
     return(
-        <div className={`nav-small ${scrolled ? "scrolled" : ""}`}>
-            <div className="ns-logo">
-                <h1 className={`ns-logo-dl ${scrolled ? "scrolled" : ""}`}>David Leder</h1>
-                <h1 className={`ns-logo-sd ${scrolled ? "scrolled" : ""}`}>[Software Developer]</h1>
+        <div className={`nav-small ${isOpen ? 'show' : ''}`}>
+            <div className={`nav-small-content ${scrolled ? "scrolled" : ""}`}>
+                <div className="ns-logo">
+                    <h1 className={`ns-logo-dl ${scrolled ? "scrolled" : ""}`}>David Leder</h1>
+                    <h1 className={`ns-logo-sd ${scrolled ? "scrolled" : ""}`}>[Software Developer]</h1>
+                </div>
+                <div className="burger" onClick={toggleBurger}>
+                    <div className={`burger-stripes ${isOpen ? 'hide' : ''}`}>
+                        <div className="burger-stripe"></div>
+                        <div className="burger-stripe"></div>
+                        <div className="burger-stripe"></div>
+                    </div>
+                    <div className={`burger-x ${isOpen ? 'show' : ''}`}>
+                        <div className="burger-x-1"></div>
+                        <div className="burger-x-2"></div>
+                    </div>
+                </div>
             </div>
-            <div className="ns-menu-cont">
-                <Burger />
+            <div className={`menu-cont ${isOpen ? 'show' : ''}`}>
+                <div className="menu">
+                    <a href="https://www.lederdavid.com/">
+                        <h2 className="menu-link">Home<div className="menu-line"></div></h2>
+                    </a>
+                    <h2 className="menu-link" onClick={() => scrollToElement("about")}>About</h2>
+                    <h2 className="menu-link" onClick={() => scrollToElement("stack")}>Tech Stack</h2>
+                    <h2 className="menu-link" onClick={() => scrollToElement("projects")}>Projects</h2>
+                    <h2 className="menu-link" onClick={() => scrollToElement("education")}>Education & Background</h2>
+                    <h2 className="menu-link" onClick={() => scrollToElement("contact")}>Contact</h2>
+                </div>
             </div>
         </div>
     );
